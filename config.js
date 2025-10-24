2210c221b3bba867c9efde0c6c42795ca5b56132
Amit Das <112541611+AmitDas4321@users.noreply.github.com>
2025-10-24 15:40:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-  PINTEREST_API_BASE: 'https://api.amitdas.site/Pinterest/api/',
+  API_BASE: 'https://api.amitdas.site/Pinterest/api/',
   WHATSAPP_INSTANCE_ID: 'WHATSAPP_INSTANCE_ID',
   WHATSAPP_ACCESS_TOKEN: 'WHATSAPP_ACCESS_TOKEN'
 };
